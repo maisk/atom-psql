67a0ccf8f91eab4b3ff43e237064c11c768e32fb
kostas Maistrelis <kostas@maistrelis.com>
2017-11-25 15:48:11 +0200

    query parser

--- spec/psql-query-parser-spec.js
+++ spec/psql-query-parser-spec.js
@@ -26,7 +26,7 @@ NOW();
   `begin; select 1 FROM "table1";
   commit;`,
 //5
- `
+  `
  CREATE OR REPLACE FUNCTION test2(arg1 integer) RETURNS varchar AS $fn$
 DECLARE
 _rep varchar;
@@ -38,24 +38,37 @@ $fn$ LANGUAGE plpgsql; select 1 from now();
  `,
 //6
   `
-  SELECT 'a',$$b$$ FROM B;
+   SELECT t1.a,t2.a FROM s1.table1 t1
+   JOIN s1.table2 t2 ON (t1.id = t2.id)
+   WHERE  t1.c in ('a',$$b$$)
+   ORDER BY t1.d
+   LIMIT 1;
+   `,
+//7
   `
+  select 
+  '$$val1 
+  val2  $ val3 $$' 
+   FROM now();
+  `,
+//8
+    `
+select 
+$$val1 
+val2 ' $ 
+val3 $$ 
+FROM now();
+`,
+
 ];
 
 let correct_test_queries_counts = [
-[1,2],[1,1],[1,1],[1,3],[2,3],[1,2],[1,1]
+  [1, 2], [1, 1], [1, 1], [1, 3], [2, 3], [1, 2], [1, 1], [1, 1], [1, 1]
 ];
 
 //6
-// `
-//   SELECT t1.a,t2.a FROM s1.table1 t1
-//   JOIN s1.table2 t2 ON (t1.id = t2.id)
-//   WHERE  t1.c in ('a',$$b$$)
-//   ORDER BY t1.d
-//   LIMIT 1;
-//   `
 
-let dump1 = function(sql) {
+let dump1 = function (sql) {
   console.log(sql);
   console.log('--');
   let qp = new PsqlQueryParser();
@@ -81,67 +94,58 @@ let dump1 = function(sql) {
 }
 
 
-
 //let qp = new PsqlQueryParser();
 
 describe('PsqlQueryParser', () => {
 
   it('test1', () => {
-     dump1(test_sqls[6]);
+    dump1(test_sqls[6]);
     console.log("━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━");
   });
   //
   it('test2', () => {
 
-    let i =0;
-    for (let sql of test_sqls){
+    let i = 0;
+    for (let sql of test_sqls) {
       let qp = new PsqlQueryParser();
       qp.addText(sql);
       let queries = qp.getQueriesExtend();
       let q = queries.length;
       let c = 0;
-      for (let query of queries){
-        c+=query['clean'].length;
+      for (let query of queries) {
+        c += query['clean'].length;
       }
       let q_ok = correct_test_queries_counts[i][0];
       let c_ok = correct_test_queries_counts[i][1];
-      console.log("#",i,'CMD_COUNT',q,'CLEAN_COUNT', c, 'oks:', q_ok, c_ok);
+      console.log("#", i, 'CMD_COUNT', q, 'CLEAN_COUNT', c, 'oks:', q_ok, c_ok);
       expect(q).toBe(q_ok);
       expect(c).toBe(c_ok);
-      i+=1;
+      i += 1;
     }
     console.log("━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━");
   });
   it('test3', () => {
 
-    let i =0;
-    for (let sql of test_sqls){
+    let i = 0;
+    for (let sql of test_sqls) {
       let qp = new PsqlQueryParser();
       qp.addText(sql);
       let queries = qp.getQueriesExtend();
       let q = queries.length;
-      let ii=0;
-      for (let query of queries){
-        ii+=1;
-        let iii=0;
-        for (let cs of query['clean']){
-          iii+=1;
-          console.log(i,ii, iii,cs);
+      let ii = 0;
+      for (let query of queries) {
+        ii += 1;
+        let iii = 0;
+        for (let cs of query['clean']) {
+          iii += 1;
+          console.log(i, ii, iii, cs);
         }
         console.log('-------------------------------------------------------------------------------');
       }
-      i+=1;
+      i += 1;
     }
     console.log("━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━");
   });
-
-
-
-
-
-
-
-
 
 
 });
