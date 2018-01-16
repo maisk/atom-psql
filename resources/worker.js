37bd5cb30bfac94ef634d6578f9276ae391b2f8a
Kostas Maistrelis <kostas@psycholate.com>
2018-01-16 17:41:27 +0200

    worker example

--- resources/worker.js
+++ resources/worker.js
@@ -2,8 +2,22 @@
 console.log("worker #1 start");
 
 
-let c= 0;
-let t1 = setInterval(function(){
-	c+=1;
-	//console.log(c);
-},1000);
+// let c= 0;
+// let t1 = setInterval(function(){
+// 	c+=1;
+// 	//console.log(c);
+// },1000);
+
+
+var i = 0;
+
+function timedCount() {
+	console.log("w",i);
+	i = i + 1;
+	postMessage(i);
+	if (i<10) {
+		setTimeout("timedCount()", 500);
+	}
+}
+
+timedCount();
